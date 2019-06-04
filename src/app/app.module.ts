eee33355ae24162cdb294028b245c343ebd61e19
Nuno Leite de Faria <nleitefaria@gmail.com>
2019-06-04 20:41:17 +0100

    Added Home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,22 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CompanyComponent } from './company/company.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CompanyComponent
+    CompanyComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-     RouterModule.forRoot([
-       { path: '', redirectTo: '/', pathMatch: 'full' },
+    RouterModule.forRoot([
+       { path: '', redirectTo: '/home', pathMatch: 'full' },
+       { path: 'home', component: HomeComponent },
        { path: 'company', component: CompanyComponent }
-     ])
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
