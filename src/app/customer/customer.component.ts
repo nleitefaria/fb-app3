2e338960b2714e7284b5cd0879138b741068eeac
nleitefaria <nleitefaria@gmail.com>
2019-06-04 14:56:14 +0100

    Update customer.component.ts

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-customer',
-  templateUrl: './customer.component.html',
-  styleUrls: ['./customer.component.css']
+  templateUrl: './customer.component.html'
 })
 export class CustomerComponent implements OnInit {
 
