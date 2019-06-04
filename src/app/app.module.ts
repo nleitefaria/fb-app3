6e1d0b8fc683a9ae183a88091074b1c34d5e785a
nleitefaria <nleitefaria@gmail.com>
2019-06-04 14:50:10 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+     RouterModule.forRoot([
+       { path: '', redirectTo: '/', pathMatch: 'full' },
+       { path: 'customers', component: CustomerComponent }
+     ])
   ],
   providers: [],
   bootstrap: [AppComponent]
